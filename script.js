7469bab2ad2caad4053ca7bc38da0998c9ccfcb8
dora-granera <32024713+dora-granera@users.noreply.github.com>
2018-06-24 14:01:40 -0700

    Added Function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]"); // enter your name here!
+  myName.html("Dora Granera"); // enter your name here!
 }
 
  /*
@@ -25,4 +25,60 @@ function draw() {
   background(0, 255, 0); // try changing this to a blue background
   fill(255, 204, 0);    // Always call fill() before the shape you want to fill in.
   rect(30, 20, 55, 55); // Draw a 30x20 pixel rectangle at coordinate (55,55)
+ drawThreeYellowCircles();
+    drawTriangle();
+ 
 }
+
+function drawThreeYellowCircles(){
+    fill("yellow");
+    ellipse(100, 100, 100, 100);
+    ellipse(150, 150, 100, 100);
+    ellipse(200, 200, 100, 100);
+    
+    
+}
+
+function drawTriangle(){
+    fill("yellow");
+     triangle(300, 100, 320, 100, 310, 80);
+}
+
+
+var angle = 0.0;
+var jitter = 0.0;
+
+function setup() {
+  createCanvas(720, 400);
+  noStroke();
+  fill(255);
+  //Draw the rectangle from the center and it will also be the
+  //rotate around that center
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(131);
+
+  // during even-numbered seconds (0, 2, 4, 6...) add jitter to
+  // the rotation
+  if (second() % 2 == 0) {  
+    jitter = random(-0.1, 0.1);
+  }
+  //increase the angle value using the most recent jitter value
+  angle = angle + jitter;
+  //use cosine to get a smooth CW and CCW motion when not jittering3
+  var c = cos(angle);
+  //move the shape to the center of the canvas
+  translate(width/2, height/2);
+  //apply the final rotation
+  rotate(c);
+  rect(0, 0, 180, 180); 
+    
+    fill("red");
+}
+
+
+ 
+
+
